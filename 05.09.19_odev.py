3dd11d000dd22921c4bba76c49e1e4b73325abb5
CnbyC <51508402+CnbyC@users.noreply.github.com>
2019-09-12 08:53:42 +0200

    Update 05.09.19_odev.py

--- 05.09.19_odev.py
+++ 05.09.19_odev.py
@@ -1,6 +1,7 @@
 class Tasit():
 
     tasit_sayisi = 0
+    __tasitlarim = []
 
     def __init__(self, motor_gucu, koltuk_sayisi, km_durumu, modeli, satis_yili, tekerlek_sayisi = 4):
         self.motor_gucu = motor_gucu
@@ -9,11 +10,11 @@ class Tasit():
         self.modeli = modeli
         self.satis_yili = satis_yili
         self.tekerlek_sayisi = tekerlek_sayisi
+        Tasit.__tasitlarim.append(self)
 
     @classmethod
-    def tasit_miktari(cls):
-        cls.tasit_sayisi += 1
-        print("Mevcut arac sayisi : ", cls.tasit_sayisi)
+    def tasit_miktari_goruntule(cls):
+        return len(cls.__tasitlarim)
 
     def koltuksayisi(self):
         print("Aracin koltuk sayisi : ", tasit1.koltuk_sayisi)
@@ -24,8 +25,40 @@ class Tasit():
     def kmdurumu(self):
         print("Aracin km'si : ", tasit1.km_durumu)
 
+    def tasit_miktari_guncelle(self):
+        Tasit.tasit_sayisi += 1
+        print("Mevcut arac sayisi : ", Tasit.tasit_sayisi)
+
+
+class Araba(Tasit):
+
+    def __init__(self, motor_gucu, koltuk_sayisi, km_durumu, modeli, satis_yili, max_hiz):
+        super().__init__(motor_gucu, koltuk_sayisi, km_durumu, modeli, satis_yili)
+        self.max_hiz=max_hiz
+
+    def arabayi_durdur(self):
+        print("Araba durdu")
+
+    def gaza_bas(self):
+        print("Araba hizlaniyor")
+
+    def arabayi_yavaslat(self):
+        print("Araba yavasliyor")
+
+    def arabanin_durumunu_goster(self):
+        if issubclass(Araba,Tasit):
+            print("Bu sinif 'Tasit' sinifindan miras alinmistir")
+        else:
+            print("Araba sinifi 'Tasit' sinifindan miras alinMAmistir")
+
+    def model_goster(self):
+        print("Araba sinifinin methodu…")
+        super().model_goster()
 
 tasit1 = Tasit(1600, 6, 76825, 2016, 2018)
+araba1 = Araba(1600, 6, 76825, 2016, 2018, 340)
+
+
 
 
 print("""Lutfen yapmak istediginiz islemi seciniz\n
@@ -33,6 +66,11 @@ print("""Lutfen yapmak istediginiz islemi seciniz\n
     Tasitin modelini gorme      2
     Tasitin km'sini gorme       3
     Tasit miktarini guncelle    4
+    Arabayi durdur              5
+    Arabayi hizlandir           6
+    Arabayi yavaslat            7
+    Arabanin modelini goster    8
+    Arabanin durumunu goster    9
 """)
 
 while True:
@@ -52,5 +90,19 @@ while True:
         tasit1.kmdurumu()
 
     elif islem == "4":
-        miktar = tasit1.tasit_sayisi
-        tasit1.tasit_miktari()
+        tasit1.tasit_miktari_guncelle()
+
+    elif islem == "5":
+        araba1.arabayi_durdur()
+
+    elif islem == "6":
+        araba1.gaza_bas()
+
+    elif islem == "7":
+        araba1.arabayi_yavaslat()
+
+    elif islem == "8":
+        araba1.model_goster()
+
+    elif islem == "9":
+        araba1.arabanin_durumunu_goster()
